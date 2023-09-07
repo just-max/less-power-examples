ec9af688fdda0d42f2d1272d4685e59926bd6fac
Max Lang <17551908+just-max@users.noreply.github.com>
2023-09-07 22:06:15 +0200

    add template

---
+++ src/assignment.ml
@@ -0,0 +1,12 @@
+type vector3 = (float * float * float)
+
+(* TODO: p1, p2, p3 *)
+let p1 = (0., 0., 0.)
+let p2 = (0., 0., 0.)
+let p3 = (0., 0., 0.)
+
+let string_of_vector3 v = failwith "TODO: string_of_vector3"
+let vector3_add a b = failwith "TODO: vector3_add"
+let vector3_max a b = failwith "TODO: vector3_max"
+
+let combine a b c = "TODO: combine"
