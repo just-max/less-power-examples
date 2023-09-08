f421149b0be208239747e7334a25dd8336b45416
Max Lang <17551908+just-max@users.noreply.github.com>
2023-09-08 16:50:50 +0200

    add Stack, Queue to template

--- examples/01-partial-type-correct/template/src/assignment.ml
+++ examples/01-partial-type-correct/template/src/assignment.ml
@@ -1,6 +1,26 @@
 (* unlike with regular exercises, the student will get feedback even from
    an empty submission, so we don't provide any template code *)
 
+
+
+module type Stack = sig
+   type 'a t
+
+   val empty : 'a t
+   val push : 'a -> 'a t -> 'a t
+   val pop : 'a t -> ('a * 'a t) option
+end
+
 (* TODO: ListStack *)
+
+
+
+module type Queue = sig
+   type 'a t
+
+   val empty : 'a t
+   val enqueue : 'a -> 'a t -> 'a t
+   val dequeue : 'a t -> ('a * 'a t) option
+end
 
 (* TODO: MakeFunctionalQueue *)
