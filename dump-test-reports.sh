fd415e6f98c5738c156d7e75808a68f065f2080d
Max Lang <17551908+just-max@users.noreply.github.com>
2023-09-09 03:36:03 +0200

    better dump

--- dump-test-reports.sh
+++ dump-test-reports.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+line='--------------------------------------------------------------------------------'
+
 xmlstarlet select --text --template \
-  --output '----------------------------------------' --nl \
-  --match '/testsuites/testsuite/testcase' \
-    --output '[' --value-of '@name' --output ']' --nl \
-    --match './error|./failure|./skipped' \
-      --value-of '.' --nl \
+  --output "$line" --nl \
+  --match '/testsuites/testsuite/testcase[not(starts-with(@name, "points:"))]' \
+    --output '[' --value-of '@name' --output ']' \
+    --if 'boolean(./error|./failure|./skipped)' \
+      --output '  ❌' --nl \
+      --match './error|./failure|./skipped' \
+        --value-of '.' --nl \
+      --break \
+    --else \
+      --output '  ✅' --nl \
     --break \
-    --output '----------------------------------------' --nl \
+    --output "$line" --nl \
     test-reports/*.xml
