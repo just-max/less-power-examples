be05f1a23ddcd5c2e1f72d19db1d9aeabf70752e
Max Lang <17551908+just-max@users.noreply.github.com>
2023-09-12 14:37:06 +0200

    01: don't hard-code exercise start/end

--- examples/01-partial-type-correct/tests/run.sh
+++ examples/01-partial-type-correct/tests/run.sh
@@ -4,7 +4,9 @@ set -e
 
 opam exec -- dune build --root tests/ --force runner/
 
-opam exec -- dune exec --root tests/ --no-build -- runner/runner.exe \
-  --exercise-start "$(date -d 'TZ="Europe/Berlin" 2023-09-01 00:00' +%s)" \
-  --exercise-end "$(date -d 'TZ="Europe/Berlin" 2023-09-30 00:00' +%s)" \
-  "$@" # pass '-s' (safe) flag from CI runner
+# use "$@" to pass '--safe', '--exercise-start' and '--exercise-end' flags from CI runner
+opam exec -- dune exec --root tests/ --no-build -- runner/runner.exe "$@"
+
+# exercise start/end can be hard-coded instead:
+# --exercise-start "$(date -d 'TZ="Europe/Berlin" 2023-09-01 00:00' +%s)"
+# --exercise-end "$(date -d 'TZ="Europe/Berlin" 2023-09-30 00:00' +%s)"
